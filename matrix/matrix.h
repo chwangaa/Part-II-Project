d910859232729042eb8310309f4b7054de5489cc
tom <wangchihang@gmail.com>
2016-01-01 14:40:31 +0000

    Refactor the strassen routine so each func call is shorter

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -3,8 +3,10 @@
 
 #include "CacheObliviousMatrixMultiplication.h"
 #include "Cblas_GEMM.h"
-#include "SimpleMatrixMultiplication.h"
-#include "StrassenMatrixMultiplication_cache_friendly_order.h"
+//#include "SimpleMatrixMultiplication.h"
+//#include "StrassenMatrixMultiplication_cache_friendly_order.h"
+#include "StrassenMatrixMultiplication_refactored.h"
+
 #include "matrix_arithmetic.h"
 #include "matrix_util.h"
 
